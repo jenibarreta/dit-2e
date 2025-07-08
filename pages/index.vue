edbd685ff66362e1d002bd0fbd13a066bd2a0a4f
paolo <castroapaolo151@gmail.com>
2025-07-08 13:36:09 +0800

    added view customer details

--- pages/index.vue
+++ pages/index.vue
@@ -2,70 +2,107 @@
   <v-row justify="center" align="center">
     <v-col cols="12">
       <v-data-table
-        :headers="headers"
-        :items="customers"
-        :items-per-page="10"
-        class="elevation-1"
-        :loading="loading"
-        loading-text="Laoding... Please wait"
-      >
-        <template v-slot:item.profilePicture="{ item }">
-
-          <v-badge
-          color="purple"
+    :headers="headers"
+    :items="customers"
+    :items-per-page="10"
+    class="elevation-1"
+    :loading="loading"
+    loading-text="Loading...Please wait"
+  >
+  <template v-slot:item.profilePicture="{ item }">
+   <v-img
+  lazy-src="https://picsum.photos/id/11/10/6"
+  max-height="80"
+  max-width="80"
+  :src="item.profilePicture"
+></v-img>
+ <v-badge 
+          color="blue"
           content="VIP"
-          v-if="items.isVip == true"
-        >
+          v-if="item.isVip == true"
+          offset-x=""
+        ></v-badge>
         <v-avatar size="60">
-          <v-img
-            lazy-src="https://picsum.photos/id/11/10/6"
-            :src="item.profilePicture"
-          ></v-img>
-        </template>
-      </v-data-table>
-    </v-col>
+        
+        </v-avatar>
+
+      </template>
+       <template v-slot:item.actions="{ item }">
+        <v-btn color="primary" @click="getCustomersDetails(item)">View</v-btn>
+       </template>
+    </v-data-table>
+
+    <v-dialog v-model="customerDialog" width="500">
+      <v-card>
+        <v-card-title><v-icon>mdi-information</v-icon>Customer Information<v-icon @click="customerDialog = false">mdi-close</v-icon></v-card-title>
+        <v-card-text class="mt-5">Information</v-card-text>
+        <v-img
+  max-height="250"
+  max-width="250"
+  src="https://picsum.photos/id/11/500/300"
+></v-img>
+        <v-card-actions></v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: "IndexPage",
+  name: 'IndexPage',
   data() {
-    return {
+    return{
       headers: [
-        {
-          align: "start",
-          sortable: false,
-        },
-        { text: "Photo", value: "profilePicture" },
-        { text: "Name", value: "name" },
-        { text: "Email ", value: "email" },
-        { text: "Phone ", value: "phone" },
-        { text: "Address", value: "address" },
-        { text: "Age", value: "age" },
-        { text: "Status", value: "isVip" },
-      ],
-      customers: [],
-      loading: true
-    };
+          {
+
+          },
+          { text: 'Photo', value: 'profilePicture' },
+          { text: 'Name', value: 'name ' },
+          { text: 'Email', value: 'email' },
+          { text: 'Phone', value: 'phone' },
+          { text: 'Address', value: 'protein' },
+          { text: 'Age', value: 'age'},
+          { text: 'Status', value: 'isVip'},
+          { text: "", value: 'actions'},
+        ],
+        customers: [],
+        loading: true,
+        customerDialog: false,
+        customerName: "",
+        customerEmail: "",
+        customerPhone: "",
+        customerAge: 0,
+        customerPhoto: ""
+    }
   },
   methods: {
     getCustomers() {
-      this.$axios
-        .get("/customers")
-        .then((response) => {
-          console.log(response.data);
-          this.customers = response.data;
-          this.loading = false
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      this.$axios.get('/customers')
+      .then(response => {
+        console.log(response.data)
+        this.customers = response.data;
+        this.loading = false
+      })
+      .catch(err => {
+        console.log(err)
+      })
     },
+
+    getCustomersDetails(item) {
+      console.log(item)
+
+    this.customerName = item.Name;
+    this.customerEmail = item.Email;  
+    this.customerPhone = item.Phone;
+    this.customerAge = item.Age;
+    this.customerPhoto = item.Photo;  
+    this.customerDialog = true;
+    }
   },
 
   mounted() {
-    this.getCustomers();
-  },
-};
+    this.getCustomers()
+  }
+}
 </script>
